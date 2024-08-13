59d4901e5621ddd0bad89e3d5f4e26ac84fdc14b
Ngoc Linh Duong <nlinh27011997@gmail.com>
2024-08-14 03:34:15 +0700

    Add files via upload

---
+++ R_Code/W2453 - Projekt2019 - Aufgabe2.R
@@ -0,0 +1,98 @@
+#####################################################################################
+#                                                                                   #
+#                       W2453 - Angewandtes Projekt mit R                           #
+#                         Aufgabe 2: Finanzmarkanalyse                              #
+#                                                                                   #
+#####################################################################################
+
+
+# Geben Sie fuer das Objekt 'MatrNr' in Zeile 8 die Matrikelnummer
+# eines Ihrer Gruppenmitglieder ein. Markieren Sie die Zeilen 8 bis 15 
+# und fuehren Sie diese zusammen aus.
+
+MatrNr = 6822695
+# Geben Sie hier Ihre Matrikelnummer ein!
+# Die Nummer eines einzelnen Gruppenmitglieds 
+# genuegt.
+setwd("/Users/ngoclinh/Desktop/Ordner+Projekt-20191218/W2453 - Projekt 2019 Gruppe XXX")
+
+source("W2453 - choose_data - nicht bearbeiten.txt")
+daten = choose_data(MatrNr) 
+U1 = daten[[1]]             # Daten fuer das erste Unternehmen
+U2 = daten[[2]]             # Daten fuer das zweite Unternehmen
+rm("daten", "choose_data")  # Löscht nicht mehr benötigte Objekte
+
+U1 #CANON
+U2 #WALMART 
+
+# Nach der Ausfuehrung des Codes finden Sie in der Konsole einen Hinweis, welche Daten 
+# fuer Ihre Projektgruppe zu bearbeiten sind.
+
+# In dem Objekt 'U1' befinden sich nun die Finanzdaten des erst-
+# genannten Unternehmens, in 'U2' hingegen die des zweiten 
+# Unternehmens.
+
+#-------------------------------------------------------------------
+# Beginnen Sie hier mit Ihrem eigenen Code:
+kurs1=U1$Close
+
+kurs2=U2$Close
+
+n1=length(kurs1)
+
+n2=length(kurs2)
+Rendite1=diff(log(kurs1))
+Rendite2=diff(log(kurs2))
+
+par(mfrow = c(2,1))	
+Jahr = (0:(n1-1))/n1*5+2014
+plot(Jahr, kurs1,type="l")
+title("Canon Aktienkurs: Mai 2014 bis April 2018")		
+
+Jahr = (0:(n1-2))/(n1-1)*5+2014
+plot(Jahr, Rendite1, type = "l")
+title("Canon - Renditen: Mai 2014 bis April 2018")
+
+par(mfrow = c(2,1))	
+Jahr = (0:(n2-1))/n2*5+2014
+plot(Jahr, kurs2,type="l")
+title("Walmart Aktienkurs: Mai 2014 bis April 2018")		
+
+Jahr = (0:(n2-2))/(n2-1)*5+2014
+plot(Jahr, Rendite2, type = "l")
+title("Walmart - Renditen: Mai 2014 bis April 2018")
+
+#b
+acf(Rendite1)
+acf(Rendite1**2)
+
+
+acf(Rendite2)
+acf(Rendite2**2)
+
+#c
+mean(Rendite1)
+
+sd(Rendite1)
+sd(Rendite1)**2
+
+
+library(TSA)							      # Aufrufen des TSA Pakets (muss vorher installiert worden sein)					
+skewness(Rendite1)		      # Schiefe der Renditen
+kurtosis(Rendite1)					# Kurtosis der Renditen
+hist(Rendite1)							# Histogramm der Renditen
+qqnorm(Rendite1)						# QQ Normal Plot der Renditen
+qqline(Rendite1)						# QQ line der Renditen
+
+mean(Rendite2)
+sd(Rendite2)
+sd(Rendite2)**2
+
+library(TSA)							      # Aufrufen des TSA Pakets (muss vorher installiert worden sein)					
+skewness(Rendite2)		      # Schiefe der Renditen
+kurtosis(Rendite2)					# Kurtosis der Renditen
+hist(Rendite2)							# Histogramm der Renditen
+qqnorm(Rendite2)						# QQ Normal Plot der Renditen
+qqline(Rendite1)						# QQ line der Renditen
+
+#-------------------------------------------------------------------
---
+++ R_Code/W2453 - Projekt2019 - Aufgabe3.R
@@ -0,0 +1,115 @@
+#####################################################################################
+#                                                                                   #
+#                       W2453 - Angewandtes Projekt mit R                           #
+#                           Aufgabe 3: GARCH-Modelle                                #
+#                                                                                   #
+#####################################################################################
+
+# Beispiel zur Nutzung des lubridate-Paketes (können Sie löschen, auskommentieren oder so lassen)
+  library(lubridate)                # load lubridate package
+  time = "15:45:23"                 # example for time (works also if "time is a vector)
+  
+  seconds = as.numeric(hms(time))   # transform the time-format to seconds
+                                    # where 00:00:00 (midnight) is 0 seconds
+  hours = seconds/60^2              # transforming seconds in decimal hours
+  
+  
+  
+ # Staten Sie Ihren Code hier
+  library(fGarch)       # lade fGarch Paket (vorher ggf. installieren)
+  #a
+  setwd("/Users/ngoclinh/Downloads")
+  K <- read.csv("ADS.DEEUR_06.01.2020.csv")
+  Kurs = K$Close    # speichere den Kurs
+  Day = K$Time  
+  seconds = as.numeric(hms(Day))
+  hours = seconds/60^2   
+  
+  n= length(Kurs)
+  
+  par(mfrow = c(2, 1))        # Öffne Plot mit zwei Zeilen
+  plot(hours, Kurs, type = "l", xlab = "Time") 
+  title("Aktienpreisreihe Addidas am 6. Januar,2020")
+  
+  
+  K1 <- read.csv("DEU.IDXEUR_06.01.2020.csv")
+  Kurs1 = K1$Close    # speichere den Kurs
+  Day1 = K1$Time  
+  seconds = as.numeric(hms(Day))
+  hours = seconds/60^2   
+  
+  n1= length(Kurs1)
+  par(mfrow = c(2, 1))        # Öffne Plot mit zwei Zeilen
+  plot(hours, Kurs1, type = "l", xlab = "Time") 
+  title("Germany 30 Index am 6. Januar,2020")
+
+  #b
+  #Addidas
+  
+  library(fGarch)       # lade fGarch Paket (vorher ggf. installieren)
+  h=length(hours)
+  h_=(1:(h-1))
+  Rendite = diff(log(Kurs))   # Berechne die log-Rendite
+  plot(h_, Rendite, type = "l", xlab = "Time")
+  title("Log-Rendite Addidas Aktienkpreis Zeitreihe, 6.Januar 2020")
+  g_model =  garchFit(~garch(1, 1), data = Rendite*100, trace = FALSE) 
+  g_model                     # Zeige das GARCH(1,1) Modell
+  g_SD=g_model@sigma.t
+  lines(h_, g_SD, col = 2) # plotte die bed. Standardabweichungen
+ 
+  
+  summary(g_model)
+  g_model@fit$coef
+ 
+  
+  h=length(hours)
+  h_=(1:(h-1))
+  g_SD=g_model@sigma.t
+  plot.ts(g_SD)
+  title("Geschätzte bedingte SD (Volatilität) für Addidas Aktienpreis Renditen")
+  
+  
+
+  #Germany Index
+  
+  Rendite1=diff(log(Kurs1))  
+  plot(h_, Rendite1, type = "l", xlab = "Time")
+  title("Log-Rendite Germany Index Zeitreihe,6.Januar 2020")
+  g_model1 =  garchFit(~garch(1, 1), data = Rendite1*100, trace = FALSE) 
+  g_model1                     # Zeige das GARCH(1,1) Modell
+  g_SD1=g_model1@sigma.t
+  lines(h_, g_SD1, col = 2) # plotte die bed. Standardabweichungen
+  
+  
+  summary(g_model1)
+  g_model1@fit$coef
+  
+  g_SD1=g_model1@sigma.t
+  plot.ts(g_SD1)
+  title("Geschätzte bedingte SD (Volatilität) für Germany Index Renditen")
+  
+
+  #c
+  Wartezeit=diff(Kurs)
+gM=  garchFit(~garch(1, 1), data = Wartezeit, trace = FALSE)
+gM  
+plot(h_,Wartezeit, type = "l", xlab = "Time")
+title("Wartezeit Addidas Aktienpreis,6.Januar.2020")
+
+Wartezeit1= diff(Kurs1)  
+gM1=garchFit(~garch(1, 1), data = Wartezeit1*100, trace = FALSE)
+gM1
+plot(h_,Wartezeit1, type = "l", xlab = "Time")
+title("Wartezeit Germany Index,6.Januar.2020")
+
+
+
+################################################
+hist(Wartezeit)
+qqnorm(Wartezeit)
+qqline(Wartezeit)
+hist(Wartezeit1)
+qqnorm(Wartezeit1)
+qqline(Wartezeit1)
+acf(Rendite)
+acf(Rendite1)
